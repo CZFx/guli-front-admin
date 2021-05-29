94fbbddc6ffe5700bb7d12f9b34dd1bf790d5d8e
sinker <201801060122@sdust.edu.cn>
2021-05-29 16:04:54 +0800

    讲师列表显示

--- src/views/edu/teacher/list.vue
+++ src/views/edu/teacher/list.vue
@@ -1,7 +1,42 @@
 <template>
   <div class="app-container">
       讲师列表
-    </div>  
+    <!--表格-->
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+    element-loading-text="数据加载中"
+    border
+    fit
+    highlight-current-row>
+
+    <el-table-column
+    label="序号"
+    width="70"
+    align="center">
+      <template slot-scope="scope">
+        {{ (page-1)*limit+scope.$index+1}}
+      </template>
+    </el-table-column>
+      <el-table-column prop="name" label="名称" width="80"/>
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{scope.row.level===1?'高级讲师':'首席讲师'}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="intro" label="资历"/>
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+      <el-table-column prop="sort" label="排序" width="60"/>
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope)"/>
+        </template>
+      </el-table-column>
+    </el-table>
+    </div>
 </template>
 
 <script>
@@ -26,9 +61,13 @@ export default ({
       teacher.getTeacherListPage(this.page,this.limit,this.teacherQuery)
         .then(response =>{
           //response接口返回的数据
-          console.log(response)
+          //console.log(response)
+          this.list=response.data.rows
+          this.total=response.data.total
+          console.log(this.list)
+          console.log(this.total)
         }) //  请求成功
-        .catch(error =>{ 
+        .catch(error =>{
           console.log(error)
         }) //请求失败
     }
